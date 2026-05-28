27777954918490c999e93745b1e94c2be7150e32
Ayush Waghmare <151551885+ayushwaghmare415@users.noreply.github.com>
2026-05-29 01:07:01 +0530

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -41,8 +41,8 @@ export const googleAuth = async (req, res) => {
     // Set cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax", 
+      secure: true",
+      sameSite: "none", 
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -61,8 +61,8 @@ export const logout = (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      secure: true",
+      sameSite: "none",
     });
 
     return res.status(200).json({
